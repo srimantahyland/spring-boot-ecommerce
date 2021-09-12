3357a2ed0f1989f710769b1def964aefd7e4f786
srimantahyland <srimanta.singh@hyland.com>
2021-09-12 23:02:17 +0530

    Adding search endpoint

--- src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -13,5 +13,7 @@ import com.luv2code.ecommerce.entity.Product;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 	
 	Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+	
+	Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
 
 }
